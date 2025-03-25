7a79aabbf9443f45f3643c4a1c9a223544f85ce5
kshithij-code <kshithijy@gmail.com>
2025-03-25 15:00:06 +0530

    stream

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, Response
+import cv2
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/")
+for i in range(5): 
+    cap = cv2.VideoCapture(i)
+    if cap.isOpened():
+        print(f"✅ Camo Studio (or another camera) detected at index {i}")
+        cap.release()
+    else:
+        print(f"❌ No camera found at index {i}")
+
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            print("Failed to read frame")
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__=="__main__":
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
     app.run(debug=True)
