53c07d179df61ca904e9070ef165034e8d29227c
kshithij <kshithijy@gmail.com>
2025-03-25 21:48:13 +0530

    Refactor video streaming to use webcam access and send frames to server; update HTML for camera integration

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
+from model import detect
 import cv2
+import numpy as np
+import base64
 
 app = Flask(__name__)
 
-camera=cv2.VideoCapture(0)
-
+# camera=cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
-        success, frame = camera.read()
-        if not success:
-            print("Failed to read frame")
-            break
-        else:
+        data = request.get_json()
+        if data and 'image' in data:
+            # Convert base64 image to numpy array
+            image_data = data['image'].split(',')[1].encode()
+            np_arr = np.frombuffer(base64.b64decode(image_data), np.uint8)
+            frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            
+            frame = detect(frame)
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
@@ -24,7 +29,8 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    frame=generate_frames()
+    return Response(frame, mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
